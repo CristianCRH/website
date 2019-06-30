0b52e7e77fcb5f541b7da3f3c6e422d4c748ab56
crh <huayanayricardo88@gmail.com>
2019-06-30 08:04:15 -0500

    configurando my web site

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+function showHide(){
+    let id=document.getElementById('container-flex');
+    let burger=document.getElementById('burger-button');
+    if(id.style.left==""||id.style.left=="-50em"){
+        
+        id.style.left="0em";
+       
+    }else
+   {
+        id.style.left="-50em";
+
+    }
+}
+
+function mygalery(id){
+    location.href="./galeria.html?id="+id;
+}
